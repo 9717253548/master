2c48b590aa2d5f7b9fbe4cd20de8cdac568c442a
9717253548 <suryavanshi2544@gmail.com>
2020-06-18 12:04:41 +0530

    fisry

--- vipin/urls.py
+++ vipin/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 # from . import simple
 
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('',include('simple.urls')),
